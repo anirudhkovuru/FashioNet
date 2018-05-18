d4cf2bcd575e836f06fb0817c6f9d9def3634c3d
anirudh.kovuru <anirudhkovuru@gmail.com>
2018-05-18 09:54:48 +0530

    Added dcgan for color images which is giving better results than the gan

--- GAN/gan.py
+++ GAN/gan.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 
 # Import keras and its various parts
-from keras.datasets import mnist
+from keras.datasets import mnist, cifar10
 from keras.layers import Input, Dense, Reshape, Flatten, Dropout
 from keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from keras.layers.advanced_activations import LeakyReLU
@@ -17,6 +17,9 @@ import sys
 # Import numpy for array operations
 import numpy as np
 
+# Import for reading images
+import cv2
+
 # GAN class
 class GAN():
 
@@ -24,15 +27,15 @@ class GAN():
     def __init__(self):
 
         # Image properties
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 1
+        self.img_rows = 32
+        self.img_cols = 32
+        self.channels = 3
 
         # Image shape
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
 
         # Size of the noise vector
-        self.latent_dim = 100
+        self.latent_dim = 200
 
         # Initialize the optimizer using Adam with learning rate 0.0002
         # and beta_01 is 0.5
@@ -51,7 +54,7 @@ class GAN():
         self.generator = self.build_generator()
 
         # The generator takes noise of size 100 as input and generates images
-        z = Input(shape=(100,))
+        z = Input(shape=(200,))
         img = self.generator(z)
 
         # For the combined model we will only train the generator
@@ -145,12 +148,12 @@ class GAN():
     def train(self, epochs, batch_size=128, sample_interval=50):
 
         # Load the dataset
-        (X_train, _), (_, _) = mnist.load_data()
+        (X_train, _), (_, _) = cifar10.load_data()
 
         # Rescale -1 to 1
         X_train = X_train / 127.5 - 1.
         # Expand dimensions to include channel data
-        X_train = np.expand_dims(X_train, axis=3)
+        # X_train = np.expand_dims(X_train, axis=3)
 
         # Adversarial ground truths
         valid = np.ones((batch_size, 1))
@@ -166,7 +169,7 @@ class GAN():
             idx = np.random.randint(0, X_train.shape[0], batch_size)
             imgs = X_train[idx]
 
-            noise = np.random.normal(0, 1, (batch_size, 100))
+            noise = np.random.normal(0, 1, (batch_size, 200))
 
             # Generate a batch of new images
             gen_imgs = self.generator.predict(noise)
@@ -180,7 +183,7 @@ class GAN():
             #  Train Generator
             # ---------------------
 
-            noise = np.random.normal(0, 1, (batch_size, 100))
+            noise = np.random.normal(0, 1, (batch_size, 200))
 
             # Train the generator
             # (to have the discriminator label samples as valid)
@@ -196,7 +199,7 @@ class GAN():
     # Method to obtain samples during training
     def sample_images(self, epoch):
         r, c = 5, 5
-        noise = np.random.normal(0, 1, (r * c, 100))
+        noise = np.random.normal(0, 1, (r * c, 200))
         gen_imgs = self.generator.predict(noise)
 
         # Rescale images to 0 - 1
@@ -206,7 +209,7 @@ class GAN():
         cnt = 0
         for i in range(r):
             for j in range(c):
-                axs[i,j].imshow(gen_imgs[cnt, :,:,0], cmap='gray')
+                axs[i,j].imshow(gen_imgs[cnt, :,:,:])
                 axs[i,j].axis('off')
                 cnt += 1
         fig.savefig("images/%d.png" % epoch)
@@ -217,6 +220,6 @@ if __name__ == '__main__':
     # Create a GAN object
     gan = GAN()
     # Training the GAN
-    gan.train(epochs=10000, batch_size=32, sample_interval=200)
+    gan.train(epochs=2000, batch_size=20, sample_interval=50)
     # Save the generator model
     gan.generator.save("saved-models/gen-model.h5")
