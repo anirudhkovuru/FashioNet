420dd52b8c80a23a869c4c37787f8e11b2b08c87
anirudhkovuru <31472818+anirudhkovuru@users.noreply.github.com>
2018-05-21 10:25:58 +0530

    Delete api.py

--- GAN/api.py
+++
@@ -1,8 +0,0 @@
-from dcgan import DCGAN
-from keras.models import load_model
-
-def get_imgs():
-    dg = DCGAN()
-    dg.combined = load_model("saved-models/combined-model.h5")
-     = 
-    for i in range(50):
