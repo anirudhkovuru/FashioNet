a803e62d79d791415312581f82f58b58f429985d
husainRaja <husain1997@gmail.com>
2018-05-22 12:26:36 +0530

    api now takes in a request as number of images and generates accordingly

--- GAN/api.py
+++ GAN/api.py
@@ -2,7 +2,7 @@ from dcgan import DCGAN
 from keras.models import load_model
 import numpy as np
 import matplotlib.pyplot as plt
-from flask import Blueprint, Flask, jsonify
+from flask import Flask, jsonify, request
 import tensorflow as tf
 
 app = Flask(__name__)
@@ -19,17 +19,23 @@ def load_gan():
     # initializing a graph to help with generating the images
     graph = tf.get_default_graph()
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods = ['GET','POST'])
 def get_images():
-    # input noise for the generator
-    noise = np.random.normal(0, 1, (1, 200))
-    # using the initialized graph to generate new images from the input noise
-    with graph.as_default():
-        gen_imgs = gan.generator.predict(noise)
-    gen_imgs = 0.5 * gen_imgs + 0.5
-    # translating into a list and converting into a JSON to send as response
-    gen_imgs = gen_imgs.tolist()
-    return jsonify({"image" :  gen_imgs})
+    json_image = {}
+    num = request.args.get('num')
+    for i in range(int(num)):
+        # input noise for the generator
+        noise = np.random.normal(0, 1, (1, 200))
+        # using the initialized graph to generate new images from the input noise
+        with graph.as_default():
+            gen_imgs = gan.generator.predict(noise)
+        gen_imgs = 0.5 * gen_imgs + 0.5
+        # translating into a list and converting into a JSON to send as response
+        gen_imgs = gen_imgs.tolist()
+        json_image["image"+str(i+1)] = gen_imgs
+
+    return jsonify(json_image)
+
 
 if __name__ == '__main__':
     load_gan()
